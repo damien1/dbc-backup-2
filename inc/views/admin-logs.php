91526d4c550426de518ec555459d25b56a247630
Damien <damien@damien.co>
2014-03-04 20:12:29 +0000

    #23 Here's the a href link to call the downloader @todo fix the hard coded URL

--- inc/views/admin-logs.php
+++ inc/views/admin-logs.php
@@ -6,6 +6,9 @@
  * Time: 14:06
  */
 
+
+
+
 if(!empty($cfg['logs'])): ?>
     <table class="widefat">
         <thead>
@@ -22,13 +25,14 @@ if(!empty($cfg['logs'])): ?>
         <tbody>
         <?php
         $i = 0;
-        foreach($cfg['logs'] AS $log): ?>
+        foreach($cfg['logs'] AS $log):
+            $dbc_file_name = ($cfg['export_dir']).'/'.basename($log['file']);?>
             <tr>
                 <td><?php echo ++$i; ?></td>
                 <td><?php echo date('Y-m-d H:i:s', $log['started']); ?></td>
                 <td><?php echo $log['status']; ?></td>
                 <td><?php echo round($log['took'], 3); ?> <?php _e('seconds', 'dbcbackup'); ?></td>
-                <td><?php echo basename($log['file']); ?></td>
+                <td><?php echo basename($log['file']); ?> <a target="_blank" href="http://dev.damien.home/wp-content/plugins/dbc-backup-2/inc/dbc_backup_downloader.php?download_file=<?php echo $dbc_file_name;?>">dl</a>  </td>
                 <td><?php echo size_format($log['size'], 2); ?></td>
                 <td><?php echo sprintf(__("%s old backups", 'dbcbackup'), intval($log['removed'])); ?></td>
             </tr>
